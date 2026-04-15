86c04a2311532b895b3cc5edf5c3d9f0da7f065d
Aanndreas <andreas.lundin@hotmail.com>
2026-04-15 08:26:00 +0200

    feat: add built-in course data

---
+++ src/data/courses.js
@@ -0,0 +1,6 @@
+export const BUILT_IN_COURSES = [
+  { id: 'mora',     name: 'Mora',     holes: 18, custom: false },
+  { id: 'orsa',     name: 'Orsa',     holes: 18, custom: false },
+  { id: 'vamhus',   name: 'Våmhus',   holes: 18, custom: false },
+  { id: 'solleron', name: 'Sollerön', holes: 18, custom: false },
+]
